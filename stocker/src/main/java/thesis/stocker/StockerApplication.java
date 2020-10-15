5f0e5f3907a70a92695f451b921be6e390c74742
patrikpasztor <patrickhu97@gmail.com>
2020-10-15 11:52:42 +0200

    Spring project generated

---
+++ stocker/src/main/java/thesis/stocker/StockerApplication.java
@@ -0,0 +1,13 @@
+package thesis.stocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockerApplication.class, args);
+	}
+
+}
---
+++ stocker/src/test/java/thesis/stocker/StockerApplicationTests.java
@@ -0,0 +1,13 @@
+package thesis.stocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
